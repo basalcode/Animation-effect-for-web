ad9fc13c50df03016c4f8bdef18c72a1ac4e85bc
basalcode <basalcode@gmail.com>
2020-04-17 23:39:22 +0900

    add : Complete to make rainbow color effect working properly
    About Changes…
    -Major
    * Rainbow color effect on Rainbow category was successfully applied.

--- src/js/rainbow.js
+++ src/js/rainbow.js
@@ -4,22 +4,20 @@ var rainbowObj = {
     colorValue : 0,
     initEvent : function (_target) {
         targetElement = _target;
-        for (var i = 0 ; i < aElement.length; i++) {
-            targetElement.addEventListener('mouseenter', this.rainbowColorOn); 
-            targetElement.addEventListener('mouseout', this.rainbowColorOff);
-        }
+        targetElement.addEventListener('mouseenter', this.rainbowColorOn); 
+        targetElement.addEventListener('mouseout', this.rainbowColorOff);
     },
 
-    rainbowColorOn : function() {
+    rainbowColorOn : function(event) {
         this.eventStop = window.setInterval(function() {
-            this.targetElement.target.style.color = 'hsl(' + anchorObj.colorValue + ', 100%, 50%)';
-            this.colorValue = (anchorObj.colorValue + 10) % 360;
+            event.target.style.color = 'hsl(' + rainbowObj.colorValue + ', 100%, 50%)';
+            rainbowObj.colorValue = (rainbowObj.colorValue + 10) % 360;
         }, 30)
     },
     
-    rainbowColorOff : function() {
-        this.colorValue = 0;
-        this.targetElement.target.style.color = 'black';
+    rainbowColorOff : function(event) {
+        rainbowObj.colorValue = 0;
+        event.target.style.color = 'black';
         clearInterval(this.eventStop);
     }
 }
