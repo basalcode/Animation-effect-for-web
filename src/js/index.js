84feeedbc3fea11674a5dd8f3c52b17b88efefd8
basalcode <basalcode@gmail.com>
2020-04-24 23:50:05 +0900

    add : Create code viewer on contents 1
    About Changes…
    -Major
    * Code-pen code veiwer was embedded on a typing content.

--- src/js/index.js
+++ src/js/index.js
@@ -21,29 +21,41 @@ function bringSidebarContent(path) {
     });
 }
 
-function bringMainContent(effectName, path) {
+function bringMainContent(effectName, path) {    
     if(path === undefined){
         return fetch('./src/data/EffectTemplate').then(function(response) {
             response.text().then(function(text){
                 document.querySelector('#main-content').innerHTML = text;
                 var effectNameList = document.querySelectorAll('span');
                 var effectField = document.querySelector('#effect-field');
+                var codeField = document.querySelector('#code-pen')
                 for (var i = 0; i < effectNameList.length; i++) {
                     effectNameList[i].innerHTML = effectName;
                 }
+                /* fectch('./src/data/Typing').then(function(response) {
+                    response.text().then(function(text){
+
+                    });
+                } */
+                
+
                 // Apply effect depending on effect.
                 switch (effectName) {
                 case 'Typing':
                     effectField.innerHTML = "";
                     textWritingAnimation(effectField, `This is an example of how ${effectName} Effect works.`);
+                    embedCode(codeField, effectName);
                     break;
                 case 'Rainbow':
                     rainbowObj.initEvent(effectField);
+                    embedCode(codeField, effectName);
                     break;
                 case 'Drop':
                     drop.dropInit(effectField);
+                    embedCode(codeField, effectName);
                     break;
                 case 'Flip':
+                    embedCode(codeField, effectName);    
                     flip.initFlip(effectField);
                     break;
                 }
@@ -65,5 +77,14 @@ if (window.location.hash) {
     bringMainContent(effectName);
 } else { // If there is no hash address.
     bringSidebarContent('./src/data/sidebar.json');
-    bringMainContent('./src/data/index');
+    bringMainContent('', './src/data/index');
+}
+
+/* embed codes from codepen */
+function embedCode(codeField, effectName) {
+    return fetch('./src/data/' + effectName).then(function(response) {
+        response.text().then(function(text){
+            codeField.innerHTML = text;
+        });
+    })
 }
